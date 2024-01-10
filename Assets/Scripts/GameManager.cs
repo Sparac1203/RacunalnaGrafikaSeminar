ec51eddd4102a0a51b7fdbbbff4b98b47f8fbcd0
Sparac1203 <sparac00@fesb.hr>
2024-01-10 18:16:29 +0100

    change between the worlds enabled

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,10 +8,19 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Player player;
     [SerializeField] private Text scoreText;
     [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject chooseWorldButton;
     [SerializeField] private GameObject gameOver;
+    [SerializeField] private MeshRenderer quadMeshRendererBackground;
+    [SerializeField] private MeshRenderer quadMeshRendererGround;
 
     private int score;
     public int Score => score;
+
+    private int currentWorld = 1; 
+    public Material world1Material;
+    public Material world2Material;
+    public Material ground1Material;
+    public Material ground2Material;
 
     private void Awake()
     {
@@ -35,13 +44,16 @@ public class GameManager : MonoBehaviour
 
         playButton.SetActive(false);
         gameOver.SetActive(false);
+        chooseWorldButton.SetActive(false);
+
 
         Time.timeScale = 1f;
         player.enabled = true;
 
         Pipes[] pipes = FindObjectsOfType<Pipes>();
 
-        for (int i = 0; i < pipes.Length; i++) {
+        for (int i = 0; i < pipes.Length; i++)
+        {
             Destroy(pipes[i].gameObject);
         }
     }
@@ -50,6 +62,7 @@ public class GameManager : MonoBehaviour
     {
         playButton.SetActive(true);
         gameOver.SetActive(true);
+        chooseWorldButton.SetActive(true);
 
         Pause();
     }
@@ -64,6 +77,23 @@ public class GameManager : MonoBehaviour
     {
         score++;
         scoreText.text = score.ToString();
+    }
+
+
+
+    public void ChangeWorld()
+    {
+        currentWorld = (currentWorld == 1) ? 2 : 1; //changing the current world
+        if (currentWorld == 1)
+        {
+            quadMeshRendererBackground.material = world1Material;
+            quadMeshRendererGround.material = ground1Material;
+        }
+        else if (currentWorld == 2)
+        {
+            quadMeshRendererBackground.material = world2Material;
+            quadMeshRendererGround.material = ground2Material;
+        }
     }
 
 }
